04db94dd2f4906f13ab6cb968f63e005db4f152a
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-01-20 21:28:41 +0200

    change base_url

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://developers.themoviedb.org/3';
+const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'b72e97d50f503cf310444389e0d21ec6';
 
 // в каждом своем файле js нужно создать экземпляр класса ApiService
@@ -19,7 +19,8 @@ export default class ApiService {
       const response = await axios.get(
         `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&page=${this.page}`
       );
-      return response;
+      const data = response.data;
+      return data;
     } catch (error) {
       console.log(error);
     }
@@ -33,7 +34,8 @@ export default class ApiService {
       const response = await axios.get(
         `${BASE_URL}/movie/${id}?api_key=${API_KEY}`
       );
-      return response;
+      const data = response.data;
+      return data;
     } catch (error) {
       console.log(error);
     }
@@ -47,7 +49,8 @@ export default class ApiService {
       const response = await axios.get(
         `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${string}&page=${this.page}`
       );
-      return response;
+      const data = response.data;
+      return data;
     } catch (error) {
       console.log(error);
     }
