79429fa7bd1e0be733f2deb3247c2255c2e44bbb
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-01-22 03:33:40 +0200

    Added js for modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,25 +1,41 @@
 import ApiService from './api.js';
+import renderModal from './templates/modal-card.js';
 
 const galleryList = document.querySelector('.gallery__list');
 const modal = document.querySelector('[data-modal]');
 const closeModalBtn = document.querySelector('[data-modal-close]');
+const filmCard = document.querySelector('.js-modal-wrap');
 
 const apiServiceModal = new ApiService();
 
-galleryList.addEventListener('click', openModal);
-closeModalBtn.addEventListener('click', toggleModal);
+let filmId;
 
-function openModal(event) {
+async function openModal(event) {
   if (
-    event.target.classList.contains('gallery__trailer') ||
-    event.target.classList.contains('trailer-text')
+    !event.target.classList.contains('gallery__trailer') &&
+    !event.target.classList.contains('trailer-text')
   ) {
-    console.log(event.target.classList.value + 'hello');
-  } else {
     toggleModal();
+    filmId = event.target.closest('li').id;
+
+    try {
+      const response = await apiServiceModal.getMovieDetails(filmId);
+      console.log(response);
+      //   const data = response.results;
+      //   console.log(data);
+
+      filmCard.innerHTML = renderModal(response);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log('hello');
   }
 }
 
 function toggleModal() {
   modal.classList.toggle('is-hidden');
 }
+
+galleryList.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', toggleModal);
