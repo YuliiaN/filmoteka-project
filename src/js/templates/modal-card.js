b05ead84b93bc07fec3a88e3a5bb9d009bdcd118
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-01-23 03:14:32 +0200

    fixed modal.scss

--- src/js/templates/modal-card.js
+++ src/js/templates/modal-card.js
@@ -11,47 +11,42 @@ export default function renderModal({
   const IMG_URL = 'https://image.tmdb.org/t/p/w500';
   const genresNames = genres.map(genre => genre.name);
   return `
-    <div class="modal-poster">
-      <img src="${IMG_URL}${poster_path}" alt="${title}" />
+    <div class="poster-wrap">
+      <img src="${IMG_URL}${poster_path}" alt="${title}" class="modal-poster"/>
     </div>
 
     <div class="modal-info">
       <h2 class="modal-title">${title}</h2>
 
       <div class="info-wrap">
-        <ul class="info-list">
-          <li class="info-item-left">
-            <p class="text-left">Vote / Votes</p>
-          </li>
-          <li class="info-item-left">
-            <p class="text-left">Popularity</p>
-          </li>
-          <li class="info-item-left">
-            <p class="text-left">Original Title</p>
-          </li>
-          <li class="info-item-left">
-            <p class="text-left">Genre</p>
-          </li>
-        </ul>
-        <ul class="info-list">
-          <li class="info-item-right">
-            <p class="text-right">
-              <span class="span-vote">${vote_average}</span>
-              <span class="span-slash">/</span>
-              <span class="span-votes">${vote_count}</span>
-            </p>
-          </li>
-          <li class="info-item-right">
-            <p class="text-right">${popularity}</p>
-          </li>
-          <li class="info-item-right">
-            <p class="text-right upper">${original_title}</p>
-          </li>
-          <li class="info-item-right">
-            <p class="text-right">${Object.values(genresNames).join(', ')}</p>
-          </li>
-        </ul>
-      </div>
+          <ul class="info-list">
+            <li class="info-item">
+              <p class="info-text left">Vote / Votes</p>
+              <p class="info-text right">
+                <span class="span-vote">${vote_average}</span>
+                <span class="span-slash">/</span>
+                <span class="span-votes">${vote_count}</span>
+              </p>
+            </li>
+
+            <li class="info-item">
+              <p class="info-text left">Popularity</p>
+              <p class="info-text right">${popularity}</p>
+            </li>
+
+            <li class="info-item">
+              <p class="info-text left">Original Title</p>
+              <p class="info-text right upper">${original_title}</p>
+            </li>
+
+            <li class="info-item">
+              <p class="info-text left">Genre</p>
+              <p class="info-text right">${Object.values(genresNames).join(
+                ', '
+              )}</p>
+            </li>
+          </ul>
+        </div>
 
       <h3 class="about">About</h3>
       <p class="annotation">
