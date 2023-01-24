954ff4b6e9aabd0c3ef1f3bea9f6ca7a9adc23e1
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-01-24 20:06:20 +0200

    add non-scrolled body on team-modal

--- src/js/team.js
+++ src/js/team.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-modal-open-team]'),
     closeModalBtn: document.querySelector('[data-modal-close-team]'),
     modal: document.querySelector('[data-modal-team]'),
+    body: document.querySelector('body'),
   };
   refs.modal.addEventListener('click', toggleModal);
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,16 +11,23 @@
   function toggleModal(e) {
     if (Object.values(refs).includes(e.target)) {
       refs.modal.classList.toggle('is-hidden');
+      onBodyToggle();
     }
   }
 
   window.addEventListener('keydown', closeModalByEscape);
 
   function closeModalByEscape(e) {
-    if (e.code === 'Escape' && !refs.modal.classList.value.includes('is-hidden')) {
+    if (
+      e.code === 'Escape' &&
+      !refs.modal.classList.value.includes('is-hidden')
+    ) {
       refs.modal.classList.toggle('is-hidden');
+      onBodyToggle();
     }
   }
+
+  function onBodyToggle() {
+    refs.body.classList.toggle('team-open');
+  }
 })();
-
-
